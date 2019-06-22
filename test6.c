04c8e8039a1adffcf365f92146a76f4c5b43fb6b
Eugene Goldyrev <eugenegoldyrev@gmail.com>
2019-06-22 15:20:26 +0300

    test6

--- test6.c
+++ test6.c
@@ -2,42 +2,51 @@
 #include<math.h>
 #include <time.h>
 int N = 10;
-double h = 0.1; //1/N
-double  eps = 0.000001;
+long double h = 0.1; //1/N
+long double  eps = 0.000001;
 int	COUNT = 0;
+long double beta = 1.0;
 
-void print_array(double array[N+1][N+1]);
-void init_array(double array[N+1][N+1]);
-void boundary_condition(double array[N+1][N+1]);
-void anal_solution(double **p, int N);
-double f_xy_00(int i, int j);
-double f_xy_minus10(int i, int j);
-double max_value(double arr[]);
-double Norm(double U[N+1][N+1], double NEW[N+1][N+1]);
-void    Jacobi(double U[N+1][N+1], double NEW[N+1][N+1]);
-char is_point_in_cyrcle(double i, double j);
+void print_array(long double array[N+1][N+1]);
+void init_array(long double array[N+1][N+1]);
+void boundary_condition(long double array[N+1][N+1]);
+void anal_solution(long double **p, int N);
+long double f_xy_00(int i, int j);
+long double f_xy_minus10(int i, int j);
+long double max_value(long double arr[]);
+long double Norm(long double U[N+1][N+1], long double NEW[N+1][N+1]);
+void    Jacobi(long double U[N+1][N+1], long double NEW[N+1][N+1]);
+char is_point_in_cyrcle(long double i, long double j);
+void left_linear_interpolation(int i, int j, long double U[N+1][N+1], long double NEW[N+1][N+1]);
+void right_linear_interpolation(int i, int j, long double U[N+1][N+1], long double NEW[N+1][N+1]);
+void up_slinear_interpolation(int i, int j, long double U[N+1][N+1], long double NEW[N+1][N+1]);
+void down__linear_interpolation(int i, int j, long double U[N+1][N+1], long double NEW[N+1][N+1]);
+long double double_left_linear_interpolation(int i, int j, long double U[N+1][N+1]);
+long double double_right_linear_interpolation(int i, int j, long double U[N+1][N+1]);
+long double double_up_linear_interpolation(int i, int j, long double U[N+1][N+1]);
+long double double_down_linear_interpolation(int i, int j, long double U[N+1][N+1]);
 
 int main(){
-  double array[N+1][N+1];
-  double new[N+1][N+1];
+  long double array[N+1][N+1];
+  long double new[N+1][N+1];
 
   init_array(array); init_array(new);
   boundary_condition(array); boundary_condition(new);
-  //print_array(array);
+  print_array(array);
   printf("\n\n\n\n\n");
   Jacobi(array, new);
   //print_array(new);
   return 0;
 }
 
-void print_array(double array[N+1][N+1]){
+void print_array(long double array[N+1][N+1]){
   int i = 0, j = 0;
         while(i < (N+1))
         {
                 j = 0;
                 while(j < (N+1))
                 {
-                        printf("%-10f ", array[i][j]);
+                        printf("%-10Lf ", array[i][j]);
                         j++;
                 }
                 printf("\n");
@@ -46,7 +55,7 @@ void print_array(double array[N+1][N+1]){
 	return ;
 }
 
-void init_array(double array[N+1][N+1]){
+void init_array(long double array[N+1][N+1]){
   int i = 1, j = 1;
 	while(i < N)
 	{	j = 1;
@@ -59,7 +68,7 @@ void init_array(double array[N+1][N+1]){
 	}
 }
 
-void boundary_condition(double array[N+1][N+1]){
+void boundary_condition(long double array[N+1][N+1]){
   int i = 0;
   int j = 0;
   while((i < (N+1)) && (j < (N + 1)))
@@ -72,23 +81,23 @@ void boundary_condition(double array[N+1][N+1]){
 	}
 }
 
-double f_xy_00(int i, int j){
+long double f_xy_00(int i, int j){
   return 0.0; //f(x,y) = 0
 }
 
-double f_xy_minus10(int i, int j){
+long double f_xy_minus10(int i, int j){
   return -10.0; //f(x,y) = -10
 }
 
-void anal_solution(double **p, int N){
+void anal_solution(long double **p, int N){
     int i,j;
     for (i=0;i<N+1;i++){
         for (j=0;j<N+1;j++){
             p[i][j] = i*j*h*h; }}
 }
 
-double max_value(double arr[]){
-	double res = 0;
+long double max_value(long double arr[]){
+	long double res = 0;
 	int c = 0;
 	res = arr[0];
 	while(c < (N+1))
@@ -100,22 +109,22 @@ double max_value(double arr[]){
 	return res;
 }
 
-double Norm(double U[N+1][N+1], double NEW[N+1][N+1]){
+long double Norm(long double U[N+1][N+1], long double NEW[N+1][N+1]){
 	int i = 0;
 	int j = 0;
 	int r = 0;
 	int c;
-	double temp[N+1][N+1];
-	double max[N+1];
-	double pre_res[N+1];
-	double res;
+	long double temp[N+1][N+1];
+	long double max[N+1];
+	long double pre_res[N+1];
+	long double res;
 	while(i < (N+1))
 	{
 		j = 0;
 		while(j < (N+1))
 		{
 			temp[i][j] = NEW[i][j] - U[i][j];
-			temp[i][j] = fabs(temp[i][j]);
+			temp[i][j] = fabsl(temp[i][j]);
 			max[j] = temp[i][j];
 			j++;
 		}
@@ -127,53 +136,85 @@ double Norm(double U[N+1][N+1], double NEW[N+1][N+1]){
 	return res;
 }
 
-void Jacobi(double U[N+1][N+1], double NEW[N+1][N+1]){
-  double tot_time = 0.0;
+char is_point_in_cyrcle(long double i, long double j){
+  char t = 't', f = 'f';
+  if (((i - 0.2)*(i - 0.2) + (j - 0.25)*(j - 0.25) - 0.04) < 0){
+    return t;
+  }
+  else return f;
+}
+
+void left_linear_interpolation(int i, int j, long double U[N+1][N+1], long double NEW[N+1][N+1]){
+  //TODO f_xy_minus10 ПОМЕНЯТЬ НА -10!!!
+  long double x = i*h, y = j*h;
+  long double tmp_value = 0.0;
+  long double delta = 0.0;
+  delta = -sqrt(0.2*0.2 - (y-0.25)*(y-0.25)) + 0.5 - (x-1);
+  tmp_value = (-U[i][j]*(delta/(h-delta))) + (-10)*(h/(h-delta));
+  NEW[i][j] = (U[i+1][j]+ tmp_value + U[i][j+1]+U[i][j-1] - h*h*(-10))/4;
+}
+
+void right_linear_interpolation(int i, int j, long double U[N+1][N+1], long double NEW[N+1][N+1]){
+  //TODO f_xy_minus10 ПОМЕНЯТЬ НА -10!!!
+  long double x = i*h, y = j*h;
+  long double tmp_value = 0.0;
+  long double delta = 0.0;
+  delta = (x+1) - sqrt(0.2*0.2 - (y-0.25)*(y-0.25)) + 0.5;
+  tmp_value = (-U[i][j]*(delta/(h-delta))) + (-10)*(h/(h-delta));
+  NEW[i][j] = (tmp_value + U[i-1][j] + U[i][j+1]+U[i][j-1] - h*h*(-10))/4;
+}
+
+void up_linear_interpolation(int i, int j, long double U[N+1][N+1], long double NEW[N+1][N+1]){
+  //TODO f_xy_minus10 ПОМЕНЯТЬ НА -10!!!
+  long double x = i*h, y = j*h;
+  long double tmp_value = 0.0;
+  long double delta = 0.0;
+  delta = (y+1) - sqrt(0.2*0.2 - (x-0.5)*(x-0.5)) + 0.25;
+  tmp_value = (-U[i][j]*(delta/(h-delta))) + (-10)*(h/(h-delta));
+  NEW[i][j] = (U[i+1][j] + U[i-1][j] + tmp_value + U[i][j-1] - h*h*(-10))/4;
+}
+
+void down__linear_interpolation(int i, int j, long double U[N+1][N+1], long double NEW[N+1][N+1]){
+  //TODO f_xy_minus10 ПОМЕНЯТЬ НА -10!!!
+  long double x = i*h, y = j*h;
+  long double tmp_value = 0.0;
+  long double delta = 0.0;
+  delta = - sqrt(0.2*0.2 - (x-0.5)*(x-0.5)) + 0.25 - (y-1);
+  tmp_value = (-U[i][j]*(delta/(h-delta))) + (-10)*(h/(h-delta));
+  NEW[i][j] = (U[i+1][j] + U[i-1][j] + U[i][j+1] + tmp_value - h*h*(-10))/4;
+}
+
+void Jacobi(long double U[N+1][N+1], long double NEW[N+1][N+1]){
+  long double tot_time = 0.0;
   time_t start_t =0, end_t =0;
   start_t = clock();
 	int i = 1;
 	int j = 1;
-  double beta = 1.0;
-	double res;
-
-  int point_position = 0;
+	long double res;
+  long double tmp_value = 0.0;
 	while(i < N)
 	{
 		j = 1;
 		while(j < N)
 		{
       if ((is_point_in_cyrcle(i*h, j*h)) == 't' ){
-        //printf("%f ", i*h);
-        if(is_point_in_cyrcle((i-1)*h, j*h) == 'f') point_position++;
-        if (is_point_in_cyrcle((i+1)*h, j*h) == 'f') point_position++;
-        if (is_point_in_cyrcle(i*h, (j+1)*h) == 'f')point_position++;
-        if (is_point_in_cyrcle(i*h, (j-1)*h) == 'f') point_position++;
-
-        printf("%d\n", point_position);
-        point_position = 0;
-        switch (point_position) {
-          case 1:
-            //printf("SLEVA TOCHKA\n");
-            break;
-          case 2:
-            printf("SPRAVA TOCHKA\n");
-            break;
-          case 3:
-           //printf("VVERHU TOCHKA\n");
-            break;
-          case 4:
-            //printf("VNIZU TOCHKA\n");
-            break;
-        }
+          if (is_point_in_cyrcle((i-1)*h, j*h) == 'f') { U[i-1][j] = double_left_linear_interpolation(i, j, U); } //NEW[i][j] = (U[i+1][j] + tmp_value + U[i][j+1]+U[i][j-1]-h*h*(-10))/4;       printf("%f\n", NEW[i][j]);}
+          if (is_point_in_cyrcle((i+1)*h, j*h) == 'f') {U[i+1][j] = double_right_linear_interpolation(i, j, U);} //NEW[i][j] = (tmp_value + U[i-1][j]+U[i][j+1]+U[i][j-1]-h*h*(-10))/4;}
+         if (is_point_in_cyrcle(i*h, (j+1)*h) == 'f') {U[i][j+1] = double_up_linear_interpolation(i, j, U);}  //NEW[i][j] = (U[i+1][j] + U[i-1][j]+tmp_value+U[i][j-1]-h*h*(-10))/4;}
+          if (is_point_in_cyrcle(i*h, (j-1)*h) == 'f') {U[i][j-1] = double_down_linear_interpolation(i, j, U);} //NEW[i][j] = (U[i+1][j] + U[i-1][j]+U[i][j+1]+tmp_value-h*h*(-10))/4;}
+          NEW[i][j] = (U[i+1][j]+U[i-1][j]+U[i][j+1]+U[i][j-1]-h*h*(-10))/4;
+          long double a = NEW[i][j];
+          //printf("%Lf\n", a);
       }
-      else {
-        NEW[i][j] = (U[i+1][j]+U[i-1][j] + pow(beta,2)*(U[i][j+1]+U[i][j-1]) - h*h*f_xy_00(i,j))/(2*(1+pow(beta,2)));
+      else{
+        NEW[i][j] = (U[i+1][j]+U[i-1][j]+U[i][j+1]+U[i][j-1])/4;
       }
 			j++;
 		}
 		i++;
 	}
-
+//print_array(NEW);
+//printf("\n\n\n\n\n");
 	//если норма больше погрешности
 	res = Norm(U, NEW);
 	if(res > eps)
@@ -195,21 +236,49 @@ void Jacobi(double U[N+1][N+1], double NEW[N+1][N+1]){
 	else
 	{
     end_t = clock();
-    tot_time = (double)(end_t - start_t)/(CLOCKS_PER_SEC);
-		printf("res = %f; COUNT = %d; time = %fs\n" , res, COUNT, tot_time);
-    //print_NEW(NEW);
+    tot_time = (long double)(end_t - start_t)/(CLOCKS_PER_SEC);
+		printf("res = %Lf; COUNT = %d; time = %Lf s\n" , res, COUNT, tot_time);
+    print_array(NEW);
 	}
 	return ;
 }
 
-char is_point_in_cyrcle(double i, double j){
-  char t = 't', f = 'f';
-  if (((i - 0.2)*(i - 0.2) + (j - 0.25)*(j - 0.25) - 0.04) < 0){
-    return t;
-  }
-  else return f;
+long double double_left_linear_interpolation(int i, int j, long double U[N+1][N+1]){
+  //TODO f_xy_minus10 ПОМЕНЯТЬ НА -10!!!
+  long double x = i*h, y = j*h;
+  long double tmp_value = 0.0;
+  long double delta = 0.0;
+  delta = -sqrt(0.2*0.2 - (y-0.25)*(y-0.25)) + 0.5 - (x-1);
+  tmp_value = (-U[i][j]*(delta/(h-delta))) + (-10)*(h/(h-delta));
+  return tmp_value;
 }
 
-void linear_interpolation(){
-  
+long double double_right_linear_interpolation(int i, int j, long double U[N+1][N+1]){
+  //TODO f_xy_minus10 ПОМЕНЯТЬ НА -10!!!
+  long double x = i*h, y = j*h;
+  long double tmp_value = 0.0;
+  long double delta = 0.0;
+  delta = (x+1) - sqrt(0.2*0.2 - (y-0.25)*(y-0.25)) + 0.5;
+  tmp_value = (-U[i][j]*(delta/(h-delta))) + (-10)*(h/(h-delta));
+  return tmp_value;
+}
+
+long double double_up_linear_interpolation(int i, int j, long double U[N+1][N+1]){
+  //TODO f_xy_minus10 ПОМЕНЯТЬ НА -10!!!
+  long double x = i*h, y = j*h;
+  long double tmp_value = 0.0;
+  long double delta = 0.0;
+  delta = (y+1) - sqrt(0.2*0.2 - (x-0.5)*(x-0.5)) + 0.25;
+  tmp_value = (-U[i][j]*(delta/(h-delta))) + (-10)*(h/(h-delta));
+  return tmp_value;
+}
+
+long double double_down_linear_interpolation(int i, int j, long double U[N+1][N+1]){
+  //TODO f_xy_minus10 ПОМЕНЯТЬ НА -10!!!
+  long double x = i*h, y = j*h;
+  long double tmp_value = 0.0;
+  long double delta = 0.0;
+  delta = - sqrt(0.2*0.2 - (x-0.5)*(x-0.5)) + 0.25 - (y-1);
+  tmp_value = (-U[i][j]*(delta/(h-delta))) + (-10)*(h/(h-delta));
+  return tmp_value;
 }
